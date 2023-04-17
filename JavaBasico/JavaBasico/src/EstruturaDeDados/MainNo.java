e1cb1962830e6c195269a9ab5a8b4c117b166b73
ooDEVoo <diegopazetto@yahoo.com.br>
2023-04-17 20:57:00 -0300

    Generics

--- JavaBasico/JavaBasico/src/EstruturaDeDados/MainNo.java
+++ JavaBasico/JavaBasico/src/EstruturaDeDados/MainNo.java
@@ -3,8 +3,33 @@ package EstruturaDeDados;
 public class MainNo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		No no1 = new No("No 1");
+		No no2 = new No("No 2");
+		
+		// Encadeamento
+		
+		no1.setProximoNo(no2);
+		
+		No no3 = new No("No 3");
+		
+		no2.setProximoNo(no3);
+		
+		No no4 = new No("No 4");
+		
+		no3.setProximoNo(no4);
+		
+		System.out.println(no1);
+		
+		System.out.println(no2);
+		System.out.println(no1.getProximoNo());
+		
+		System.out.println("---------");
+		
+		System.out.println(no1.getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+	
 	}
 
 }
